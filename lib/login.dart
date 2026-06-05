0e64043d540f6791d1c90e6141347501b5460454
“ResennDev” <guilherme_dutra23@hotmail.com>
2026-06-05 16:46:32 -0300

    Add theme configuration and initial app structure
    - Created a new file `theme.dart` to define the app's theme using Material Design and Google Fonts.
    - Implemented a `MyApp` class that sets up the MaterialApp with a dark theme and a login page as the home widget.

--- lib/login.dart
+++ lib/login.dart
@@ -6,60 +6,131 @@ String senha = '';
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
-  @override
-  _LoginPageState createState() => _LoginPageState();
+@override
+State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: Padding(padding: EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              onChanged: (text) {
-                email = text;
-              },
-              keyboardType: TextInputType.emailAddress,
-              decoration:
-                InputDecoration(border:OutlineInputBorder(),
-                labelText: 'Email'
-                ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextField(
-              onChanged: (text) {
-                senha = text;
-              },
-              obscureText: true,
-              decoration:
-                InputDecoration(border:OutlineInputBorder(),
-                labelText: 'Senha'
-                ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            ElevatedButton(
-              child: Text('Entrar'),
-              onPressed: () {
-                  if (email == 'admin' && senha == 'admin') {
-                      print('Login bem-sucedido!');
-                  } else {
-                      print('Email ou senha incorretos.');
-                  }
-              },
-          ),
-      ])
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: 300,
+          height: 450,
 
-        )
-    ));
+          decoration: BoxDecoration(
+          color: Colors.green,
+          borderRadius: BorderRadius.circular(25),
+        ),
+
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 10),
+
+              Icon(Icons.login,
+              size: 60,
+              color: Colors.white,),
+
+                SizedBox(height: 10),
+
+              Text('Faça seu login', 
+              style: TextStyle(fontSize: 24, 
+              fontWeight: FontWeight.bold, 
+              color: Colors.white),),
+
+                SizedBox(height: 20),
+
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text('Email', 
+                style: TextStyle(
+                  fontWeight: FontWeight.bold, 
+                  color: Colors.white),),
+              ),
+
+              TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: const Color.fromARGB(255, 28, 77, 29),
+
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 5),
+
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text('Senha', 
+                style: TextStyle(
+                  fontWeight: FontWeight.bold, 
+                  color: Colors.white),),
+              ),
+
+              TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: const Color.fromARGB(255, 28, 77, 29),
+
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 20),
+
+              SizedBox(
+                width:double.infinity,
+
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.green,
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                  ),
+                  child: Text('Entrar', 
+                  style: TextStyle(fontSize: 18, 
+                  fontWeight: FontWeight.bold),),
+                ),
+              ),
+
+              SizedBox(height: 5),              
+
+              Text(
+              'Não é cadastrado?',
+              style: TextStyle(
+                color: Colors.white
+              ),),
+
+              SizedBox(height: 3),
+
+              Text(
+                'Cadastre-se',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.underline,
+                ),),
+
+
+            ],
+          ),
+        ),
+      ),
+    ),);
   }
 }
---
+++ lib/theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:projeto_app/login.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: _buildTheme(Brightness.dark),
+      home: LoginPage(),
+    );
+  }
+}
+
+ThemeData _buildTheme(Brightness brightness) {
+  final baseTheme = ThemeData(brightness: brightness);
+  return baseTheme.copyWith(
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.deepPurple,
+      brightness: brightness,
+    ),
+    textTheme: GoogleFonts.oswaldTextTheme(baseTheme.textTheme),
+  );
+}
