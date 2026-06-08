c328c4eae4cf6dd551301fac6a8679e6e10dc12b
ResennDev <guilherme_dutra23@hotmail.com>
2026-06-08 15:36:47 -0300

    Refactor LoginPage layout for improved styling and structure

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_app/theme.dart';
 
 String email = '';
 String senha = '';
@@ -6,8 +7,8 @@ String senha = '';
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
-@override
-State<LoginPage> createState() => _LoginPageState();
+  @override
+  State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
@@ -20,117 +21,150 @@ class _LoginPageState extends State<LoginPage> {
           height: 450,
 
           decoration: BoxDecoration(
-          color: Colors.green,
-          borderRadius: BorderRadius.circular(25),
-        ),
+            color: Colors.green,
+            borderRadius: BorderRadius.circular(10),
+          ),
 
-        child: Padding(
-          padding: EdgeInsets.all(20),
-          
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(height: 10),
+          child: Padding(
+            padding: EdgeInsets.all(20),
 
-              Icon(Icons.login,
-              size: 60,
-              color: Colors.white,),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 10),
+
+                Container(
+                  width: 70,
+                  height: 70,
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(200, 28, 77, 29),
+                    shape: BoxShape.circle,
+                  ),
+
+                  child: Icon(Icons.login, size: 55, color: Colors.green),
+                ),
 
                 SizedBox(height: 10),
 
-              Text('Faça seu login', 
-              style: TextStyle(fontSize: 24, 
-              fontWeight: FontWeight.bold, 
-              color: Colors.white),),
+                Text(
+                  'Faça seu login',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
 
                 SizedBox(height: 20),
 
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text('Email', 
-                style: TextStyle(
-                  fontWeight: FontWeight.bold, 
-                  color: Colors.white),),
-              ),
-
-              TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: const Color.fromARGB(255, 28, 77, 29),
-
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-
-              SizedBox(height: 5),
-
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text('Senha', 
-                style: TextStyle(
-                  fontWeight: FontWeight.bold, 
-                  color: Colors.white),),
-              ),
-
-              TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: const Color.fromARGB(255, 28, 77, 29),
-
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-
-              SizedBox(height: 20),
-
-              SizedBox(
-                width:double.infinity,
-
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    foregroundColor: Colors.green,
-                    padding: EdgeInsets.symmetric(vertical: 15),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(25),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Email',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
                     ),
                   ),
-                  child: Text('Entrar', 
-                  style: TextStyle(fontSize: 18, 
-                  fontWeight: FontWeight.bold),),
                 ),
-              ),
 
-              SizedBox(height: 5),              
+                TextField(
+                  style: TextStyle(color: Colors.white70),
+                  decoration: InputDecoration(
+                    hintText: 'Digite seu email',
+                    hintStyle: TextStyle(color: Colors.white70),
 
-              Text(
-              'Não é cadastrado?',
-              style: TextStyle(
-                color: Colors.white
-              ),),
+                    filled: true,
+                    fillColor: const Color.fromARGB(255, 28, 77, 29),
 
-              SizedBox(height: 3),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
 
-              Text(
-                'Cadastre-se',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  decoration: TextDecoration.underline,
-                ),),
+                SizedBox(height: 5),
 
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Senha',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
 
-            ],
+                TextField(
+                  obscureText: true,
+                  style: TextStyle(color: Colors.white70),
+                  decoration: InputDecoration(
+                    hintText: 'Digite sua senha',
+                    hintStyle: TextStyle(color: Colors.white70),
+
+                    filled: true,
+                    fillColor: const Color.fromARGB(255, 28, 77, 29),
+
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 20),
+
+                SizedBox(
+                  width: double.infinity,
+
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors.green,
+                      elevation: 8,
+                      padding: EdgeInsets.symmetric(vertical: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                    ),
+                    child: Text(
+                      'Entrar',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 5),
+
+                Text(
+                  'Não é cadastrado?',
+                  style: TextStyle(color: Colors.white),
+                ),
+
+                SizedBox(height: 3),
+
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Cadastre-se',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
-    ),);
+    );
   }
 }
